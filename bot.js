81a2b02e2192359fa384be5f5e5a98c02ea10603
akshi159 <akshitchaubey70@gmail.com>
2026-02-06 11:37:46 +0530

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,53 @@
+const TelegramBot = require('node-telegram-bot-api');
+const { google } = require('googleapis');
+
+// TELEGRAM BOT TOKEN
+const token = '8392335540:AAHnIzouqVA2kOm9Em0FtLsu0K8dVhfwys0';
+
+const bot = new TelegramBot(token, { polling: true });
+
+// GOOGLE AUTH
+const auth = new google.auth.GoogleAuth({
+  keyFile: 'telegram-sales-bot.json',
+  scopes: ['https://www.googleapis.com/auth/spreadsheets']
+});
+
+// GOOGLE SHEET ID
+const spreadsheetId = '19zjTH-HpP95CNjfrePRVWpf_eRRIl3xO-hyVyaBC110';
+
+console.log("BOT STARTED");
+
+bot.on('message', async (msg) => {
+
+  const text = msg.text;
+
+  if(text && text.startsWith('#lead')) {
+
+    try {
+
+      const data = text.replace('#lead ', '').split('|');
+
+      const pocBrand = data[0].trim();
+      const segment = data[1].trim();
+      const comments = data[2].trim();
+
+      const client = await auth.getClient();
+      const sheets = google.sheets({ version: 'v4', auth: client });
+
+      await sheets.spreadsheets.values.append({
+        spreadsheetId,
+        range: 'Lead!A:C',
+        valueInputOption: 'RAW',
+        resource: {
+          values: [[pocBrand, segment, comments]]
+        }
+      });
+
+      bot.sendMessage(msg.chat.id, '✅ Lead added to sheet');
+
+    } catch(error) {
+      console.log(error);
+      bot.sendMessage(msg.chat.id, '❌ Error adding lead');
+    }
+  }
+});
